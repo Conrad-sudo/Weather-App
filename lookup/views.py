fcc587889688754e08403e30739af4c625d14c26
Conrad-sudo <conrad.japhet@gmail.com>
2023-04-12 18:25:38 +0300

    added api to home view

--- lookup/views.py
+++ lookup/views.py
@@ -5,8 +5,24 @@ from django.shortcuts import render
 #This is the views file
 
 #Points a request to the webpage that we made
+
+
 def home(request):
-    return render(request,'home.html',{})
+    import json
+    import requests
+    url="https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=13E04B77-23D5-4D90-A60E-CF11884B87AD"
+    api_request=requests.get(url)
+
+    try:
+        api_response=json.loads(api_request.content)
+    except Exception as e:
+        api_response="Error..."
+
+
+
+
+
+    return render(request,'home.html',{"api":api_response})
 
 
 def about(request):
