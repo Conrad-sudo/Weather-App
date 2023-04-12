0dbcc3c8ffeed5f8f15636c16373babf5814caab
Conrad-sudo <conrad.japhet@gmail.com>
2023-04-12 17:08:17 +0300

    added comment to views.py

--- lookup/views.py
+++ lookup/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#This is the views file
+
 #Points a request to the webpage that we made
 def home(request):
     return render(request,'home.html',{})
